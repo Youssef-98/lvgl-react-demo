d3a44066abe0ec6d4e29d2779d6af94b2cc0008b
Youssef El Hindaz <s111023@ap.be>
2025-10-26 01:10:02 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Header from './components/Header'
+
+
 function App() {
   return (
-    <div>
-      <h2 className="text-2xl text-emerald-500">Hello World</h2>
-    </div>
+    <>
+      <Header />
+
+      {/* Default page padding so content doesn't touch the viewport edges */}
+      <main className='dark:bg-gray-900 min-h-screen flex justify-center px-6'>
+        <h2 className="text-2xl text-zinc-50">Hello World</h2>
+      </main>
+    </>
   )
 }
 
